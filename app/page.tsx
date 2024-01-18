9d8efe24d2581373f09a55f8c6947a8c586fb982
cbtab <136747826+cbtab@users.noreply.github.com>
2024-01-18 12:27:50 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+  console.log(process.env.NODE_ENV)
   return (
     <main className={styles.main}>
       <div className={styles.description}>
